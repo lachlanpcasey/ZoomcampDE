f8b9ace28f62637606fa96512b7a00b0734454fd
lachlanpcasey <122522521+lachlanpcasey@users.noreply.github.com>
2023-02-07 00:47:56 +1100

    Create Week 2 Q5.py

---
+++ Week 2 Q5.py
@@ -0,0 +1,69 @@
+#Script to migrate april 2019 data for green taxis to BQ to see how many rows it has
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+
+
+@task(retries=3)
+def extract_from_gcs(color: str, year: int, month: int) -> Path:
+    """Download trip data from GCS"""
+    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
+    # gcs_path = f"{color}_tripdata_{year}-{month:02}.parquet"
+    gcs_block = GcsBucket.load("zoom-gcs")
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"../data/")
+    return Path(f"../data/{gcs_path}")
+
+# Remove tasks that cleans data
+@task()
+def transform(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    df = pd.read_parquet(path)
+    num_rows = len(df)
+    # print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    # df["passenger_count"].fillna(0, inplace=True)
+    # print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+    print(num_rows)
+    return df
+
+
+@task()
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BiqQuery"""
+
+    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+
+    df.to_gbq(
+        destination_table="dezoomcamp.green_trip_4_2019",
+        project_id="dtc-de-updated",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append",
+    )
+
+
+
+@flow()
+def etl_gcs_to_bq(year: int, month: int, color:str)->None:
+    """Main ETL flow to load data into Big Query"""
+    # color = "yellow"
+    # year = 2019
+    # month = [2,3]
+    path = extract_from_gcs(color, year, month)
+    df = transform(path)
+    write_bq(df)
+
+@flow()
+def etl_parent_flow(
+    months: list[int] = [4], year: int = 2019, color:str = "green"
+):
+    for month in months:
+        etl_gcs_to_bq(year, month, color)
+
+
+if __name__ == "__main__":
+    color = "green"
+    months = [4]
+    year = 2019
+    etl_parent_flow(months, year, color)
